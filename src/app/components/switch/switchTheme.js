3f710a0c77054887af289ff9fd7cc9196392c07b
florent <florent.sauvetage@gmail.com>
2023-05-26 11:16:43 +0200

    fix: client side switch theme

--- src/app/components/switch/switchTheme.js
+++ src/app/components/switch/switchTheme.js
@@ -1,8 +1,8 @@
+"use client";
+
 import Image from "next/image";
 
 import { useContext } from "react";
-
-import flowEventsLogo from "public/Flow.jpg";
 
 import { CustomThemeContext } from "./../../context/WrapperContext";
 
